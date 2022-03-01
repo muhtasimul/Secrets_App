1b3d32ced3cb10a8945030210979e2764afb1627
MD Huq <muhtasimul@outlook.com>
2022-02-28 21:07:57 -0800

    Nineteenth Commit

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,15 @@
 import MainPage from "../components/MainComponents/MainPage"
+import Head from "next/head";
 
 export default function HomePage() {
     return (
         <>
+            <Head>
+                <title>
+                    Secrets App
+                </title>
+                <meta name="description" content="Encrypt your messages here." />
+            </Head>
             <MainPage />
         </>
     )
--- pages/results/index.js
+++ pages/results/index.js
@@ -1,8 +1,16 @@
 import ResultPage from "../../components/MainComponents/ResultPage";
+import Head from "next/head";
 
 export default function Results() {
     return (
         <>
+            <Head>
+                <title>
+                    Confidential Message
+                </title>
+                <meta name="description" content="Your confidential Message" />
+
+            </Head>
             <ResultPage />
         </>
     )
